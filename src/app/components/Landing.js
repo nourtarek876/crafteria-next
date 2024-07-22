5dd956c9963c80f86bce2c4c1a70294e693106c4
nourtarek876 <nourtarek868@gmail.com>
2024-07-22 21:44:41 +0300

    fix card link try 4

--- src/app/components/Landing.js
+++ src/app/components/Landing.js
@@ -7,17 +7,17 @@ const Landing = () => {
         <Navbar />
        {/* start landing */}
        <div className="landing">
-          <div class="landing-text">
+          <div className="landing-text">
             <h1>Top Rating</h1>
             <h2>Explore Best Crafts</h2>
           </div>
           <div className="card-main">
             <div className="cards">
               <div className="first-card">
-                <div class="card1">
-                  <div class="card-image"></div>
-                  <p class="card-title">Ahmed Nour (Furniture)</p>
-                  <p class="card-body">
+                <div className="card1">
+                  <div className="card-image"></div>
+                  <p className="card-title">Ahmed Nour (Furniture)</p>
+                  <p className="card-body">
                     Nullam ac tristique nulla, at convallis quam. Integer
                     consectetur mi nec magna tristique, non lobortis.
                   </p>
@@ -27,10 +27,10 @@ const Landing = () => {
                 </div>
               </div>
               <div className="secound-card">
-                <div class="card1">
-                  <div class="card-image"></div>
-                  <p class="card-title">Kaled Amr (Pottery)</p>
-                  <p class="card-body">
+                <div className="card1">
+                  <div className="card-image"></div>
+                  <p className="card-title">Kaled Amr (Pottery)</p>
+                  <p className="card-body">
                     Nullam ac tristique nulla, at convallis quam. Integer
                     consectetur mi nec magna tristique, non lobortis.
                   </p>
@@ -40,10 +40,10 @@ const Landing = () => {
                 </div>
               </div>
               <div className="third-card">
-                <div class="card1">
-                  <div class="card-image"></div>
-                  <p class="card-title">Omar Ahmed (Copper)</p>
-                  <p class="card-body">
+                <div className="card1">
+                  <div className="card-image"></div>
+                  <p className="card-title">Omar Ahmed (Copper)</p>
+                  <p className="card-body">
                     Nullam ac tristique nulla, at convallis quam. Integer
                     consectetur mi nec magna tristique, non lobortis.
                   </p>
@@ -57,10 +57,10 @@ const Landing = () => {
           <div className="card-main">
             <div className="cards">
               <div className="first-card">
-                <div class="card1">
-                  <div class="card-image"></div>
-                  <p class="card-title">Ahmed Nour (Furniture)</p>
-                  <p class="card-body">
+                <div className="card1">
+                  <div className="card-image"></div>
+                  <p className="card-title">Ahmed Nour (Furniture)</p>
+                  <p className="card-body">
                     Nullam ac tristique nulla, at convallis quam. Integer
                     consectetur mi nec magna tristique, non lobortis.
                   </p>
@@ -70,10 +70,10 @@ const Landing = () => {
                 </div>
               </div>
               <div className="secound-card">
-                <div class="card1">
-                  <div class="card-image"></div>
-                  <p class="card-title">Kaled Amr (Pottery)</p>
-                  <p class="card-body">
+                <div className="card1">
+                  <div className="card-image"></div>
+                  <p className="card-title">Kaled Amr (Pottery)</p>
+                  <p className="card-body">
                     Nullam ac tristique nulla, at convallis quam. Integer
                     consectetur mi nec magna tristique, non lobortis.
                   </p>
@@ -83,10 +83,10 @@ const Landing = () => {
                 </div>
               </div>
               <div className="third-card">
-                <div class="card1">
-                  <div class="card-image"></div>
-                  <p class="card-title">Omar Ahmed (Copper)</p>
-                  <p class="card-body">
+                <div className="card1">
+                  <div className="card-image"></div>
+                  <p className="card-title">Omar Ahmed (Copper)</p>
+                  <p className="card-body">
                     Nullam ac tristique nulla, at convallis quam. Integer
                     consectetur mi nec magna tristique, non lobortis.
                   </p>
