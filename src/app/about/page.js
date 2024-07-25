c70884d948d96dd74bb4eb21d2ab5833d864aa10
nourtarek876 <nourtarek868@gmail.com>
2024-07-26 02:36:49 +0300

    add about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function about(){
@@ -5,9 +6,19 @@ export default function about(){
         <>
         <Navbar />
         <div className="about-page">
-            <div className="user-sec"></div>
-            <div className=""></div>
+            <div className="user-sec">
+                <h1>For Buyer:</h1>
+                <p>All Hand Crafts You Need In one Place.</p>
+                <p>Exclusive Offers For Buyers From Our website.</p>
+                <p>Easy Find to Your Best Gadget.</p>
+            </div>
+            <div className="port-sec">
+                <h1>For Seller:</h1>
+                <p>Free 4 Month Pro Portfolio.</p>
+                <p>Easy Access to your Buyers.</p>
+            </div>
         </div>
+        <Footer />
         </>
     )
 }
